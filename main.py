c1abd968c27a4eecdc69a83e09af7cb12003f784
kohiry <kohiry@mail.ru>
2023-08-13 15:41:47 +0400

    done first method

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import uvicorn
 from fastapi import FastAPI
 
-print(1)
+import script
+
+app = FastAPI(title="Geo name")
+
+
+@app.get("/{geonameid}")
+def read_root(geonameid: str):
+    return script.get(geonameid)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
--- script.py
+++ script.py
@@ -1,17 +1,41 @@
-import time
+# import time
+
+
+def transform(line: str):
+    list_line = line.split("\t")
+    answer = {
+        "geonameid": list_line[0],
+        "name": list_line[1],
+        "asciiname": list_line[2],
+        "alternatenames": list_line[3],
+        "latitude": list_line[4],
+        "longitude": list_line[5],
+        "feature class": list_line[6],
+        "feature code": list_line[7],
+        "cc2": list_line[8],
+        "admin1 code": list_line[9],
+        "admin2 code": list_line[10],
+        "admin3 code": list_line[11],
+        "admin4 code": list_line[12],
+        "population": list_line[13],
+        "elevation": list_line[14],
+        "dem": list_line[15],
+        "timezone": list_line[16],
+        "modification date": list_line[17],
+    }
+    return answer
 
 
 def get(geonameid: str) -> None:
-    starttime = time.time()
+    # starttime = time.time()
     with open("RU.txt", "r") as f:
         line = f.readline()
         while line:
             # print(line.split("\t")[0])
             if line.split("\t")[0] == geonameid:
-                print("Find!")
+                # print("Find!")
+                return transform(line)
             line = f.readline()
-    endtime = time.time()
-    print(endtime - starttime)
 
-
-get('12547726')
+    # endtime = time.time()
+    # print(endtime - starttime)
